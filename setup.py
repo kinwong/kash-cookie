0157537dda7bf7ad4f49c2263e16909599135d6f
kinwong <kinhangwong@gmail.com>
2017-02-04 01:25:11 +0800

    Keep the core folder.

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-
 from setuptools import setup, find_packages
 
-
 with open('README.rst') as f:
-    readme = f.read()
+    README = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    LICENSE = f.read()
 
 setup(
     name='kash-cookie',
     version='0.1.0',
     description='Generic stock time series',
-    long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
+    long_description=README,
+    author='Kin Wong',
+    author_email='kinhangwong@gmail.com',
     url='',
-    license=license,
+    license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs')))
 
 
